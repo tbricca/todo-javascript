2c9f97242fcaa337b6f09651b97dd2f6bf8a8d18
tbricca <tbricca@gmail.com>
2018-09-17 16:05:50 -0700

    started to make it so i can remove tasks

--- main.js
+++ main.js
@@ -14,8 +14,10 @@
 //   let value = document.getElementById("item").value;
 // });
 
+/// let list = an array
 let list = [];
 
+// be able to explain this out
 const addToList = () => {
   let newTask = document.getElementById("new-task");
   let todolist = document.getElementById("list");
@@ -24,10 +26,21 @@ const addToList = () => {
   // Add New Task text to Item ----------
   item.innerText = newTask.value;
 
+  // Add Remove Functionality to item
+  item.onclick = removeFromList.bind(this, list.length);
+
   // Add Item to List
   todolist.appendChild(item);
   list.push(newTask.value);
 
   // Reset the Text -------
   newTask.value = "";
+};
+
+const removeFromList = index => {
+  let todoList = document.getElementById("list");
+  console.log(index);
+  list.splice(index, 1);
+
+  todoList.innerHTML = "";
 };
